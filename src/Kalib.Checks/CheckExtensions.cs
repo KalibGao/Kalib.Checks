5098c0ecd37a41dc2564c68328d36f8f077cfc17
kalibgao <gaoziyin@vip.163.com>
2018-04-02 15:38:36 +0800

    Initial Project and Test

---
+++ src/Kalib.Checks/CheckExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kalib.Checks
+{
+    public static class CheckExtensions
+    {
+        public static void CheckNull<T>(this T item, string arugmentName = null)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(arugmentName ?? nameof(item));
+            }
+        }
+
+        public static void CheckEmpty<T>(this IReadOnlyList<T> list, string argumentName = null)
+        {
+            list.CheckNull(argumentName);
+            if (list.Count == 0)
+            {
+                throw new ArgumentException($"collection must contain at least one item.", argumentName ?? nameof(list));
+            }
+        }
+
+        public static void CheckEmpty(this string value, string argumentName = null)
+        {
+            value.CheckNull(argumentName);
+
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("value can not be empty.", argumentName ?? nameof(value));
+            }
+        }
+
+        public static void CheckWhitespace(this string value, string argumentName = null)
+        {
+            value.CheckNull(argumentName);
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException($"value can not be empty or whitespace.", argumentName ?? nameof(value));
+            }
+        }
+    }
+}
---
+++ tests/Kalib.Checks.Test/CheckExtensionsTest.cs
@@ -0,0 +1,43 @@
+using System;
+using Xunit;
+using Kalib.Checks;
+
+namespace Kalib.Checks.Test
+{
+    public class CheckExtensionsTest
+    {
+        [Fact]
+        public void CheckNull()
+        {
+            string value = null;
+            Assert.Throws<ArgumentNullException>(() => value.CheckNull(nameof(value)));
+        }
+
+        [Fact]
+        public void CheckEmpty()
+        {
+
+            string value = null;
+            Assert.Throws<ArgumentNullException>(() => value.CheckEmpty(nameof(value)));
+
+            value = "";
+            Assert.Throws<ArgumentException>(() => value.CheckEmpty(nameof(value)));
+        }
+
+
+        [Fact]
+        public void CheckWhitespace()
+        {
+
+            string value = null;
+            Assert.Throws<ArgumentNullException>(() => value.CheckEmpty(nameof(value)));
+
+            value = " ";
+            Assert.Throws<ArgumentException>(() => value.CheckWhitespace(nameof(value)));
+        }
+
+  
+
+
+    }
+}
